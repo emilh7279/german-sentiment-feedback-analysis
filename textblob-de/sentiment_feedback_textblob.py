82b504360776efb526ebfcc16873313d2ce01521
Emil Hoffmann <emilh7279@gmail.com>
2025-03-06 19:27:21 +0100

    Chnaged function names insentiment_feedback_textblob.py
    Changed function names from german to englisch

--- textblob-de/sentiment_feedback_textblob.py
+++ textblob-de/sentiment_feedback_textblob.py
@@ -5,74 +5,73 @@ from pathlib import Path
 import pandas as pd
 from textblob_de import TextBlobDE as TextBlob
 
-def satz_bewertung(satz):
+def sentence_classification(sentence):
     """
     Bewertet die Stimmung eines deutschen Satzes (positiv, negativ oder neutral).
     Mithilfe des textblob_de Modul
     """
     # Erstellen eines TextBlob-Objekts
-    blob = TextBlob(satz)
+    blob = TextBlob(sentence)
 
     # Polarität des Satzes analysieren
-    polarität = blob.sentiment.polarity
+    polarity = blob.sentiment.polarity
 
-    # Stimmungsbewertung basierend auf Polarität
-    if polarität > 0:
-        return "positiv", polarität
-    elif polarität < 0:
-        return "negativ", polarität
+    # Stimmungsbewertung basierend auf polarity
+    if polarity > 0:
+        return "positiv", polarity
+    elif polarity < 0:
+        return "negativ", polarity
     else:
-        return "neutral", polarität
+        return "neutral", polarity
 
-def lade_saetze(dateiname):
+def load_sentences(filename):
     """
     Mithilfe der pandas Funktion read_csv wird das csv-File in ein pandas Dataframe
     geladen und die Spalte mit den enthaltenen Feedback-Texten in das List-Objekt
     saetze geschrieben. Das List-Objekt wird an die Funktion zurückgegeben.
     """
-    arbeitsverzeichniss = Path.cwd()
-    daten_verzeichnis = str(arbeitsverzeichniss.parent) + r"\input_data"
-    # dateiname = 'feedback.csv'
-    file_to_open = os.path.join(daten_verzeichnis, dateiname)
+    working_dir = Path.cwd()
+    data_dir = str(working_dir.parent) + r"\input_data"
+    file_to_open = os.path.join(data_dir, filename)
     df = pd.read_csv(file_to_open, delimiter=";")
-    saetze = df['Feedback_Text'].tolist()
-    return saetze
+    sentences = df['Feedback_Text'].tolist()
+    return sentences
 
-def analysiere_saetze(saetze):
+def analyse_sentences(sentences):
     """
     Funktion: Analysiert eine Liste von Sätzen und gibt die Ergebnisse zurück.
     iteriert über die Sätze aus der Liste / pandas Dataframe und übergibt sie an Funktion
     satz_bewertung(). Das List objekt ergebnisse wird bei jeder Iteration erweitert
     """
-    ergebnisse = []
-    for satz in saetze:
-        stimmung, polarität = satz_bewertung(satz)
-        ergebnisse.append((satz, stimmung, polarität))
-    return ergebnisse
+    results = []
+    for sentence in sentences:
+        mood, polarity = sentence_classification(sentence)
+        results.append((sentence, mood, polarity))
+    return results
 
-def speichere_ergebnisse(ergebnisse):
+def save_results(results):
     """
     Funktion: Speichert die Ergebnisse der Analyse in einer CSV-Datei.
     """
     # Deklariere Dateinamen-Variablen
     datum = dt.datetime.now().strftime("%Y%m%d_%HT%M")
     result_pfad = "results/"
-    dateiname = result_pfad + str(datum) + "_Ergebnisse.csv"
+    filename = result_pfad + str(datum) + "_Ergebnisse.csv"
 
     # Schreibe CSV Ergebiss-Datei
-    with open(dateiname, mode='w', newline='', encoding='utf-8') as file:
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(["Satz", "Stimmung", "Polarität"])
-        for satz, stimmung, polarität in ergebnisse:
-            writer.writerow([satz, stimmung, polarität])
+        writer.writerow(["sentence", "mood", "polarity"])
+        for sentence, mood, polarity in results:
+            writer.writerow([sentence, mood, polarity])
 
 # Lade Sätze
-saetze =  lade_saetze("feedback.csv")
+sentences =  load_sentences("feedback.csv")
 
 # Sätze analysieren
-ergebnisse = analysiere_saetze(saetze)
+results = analyse_sentences(sentences)
 
 # Ergebnisse speichern
-speichere_ergebnisse(ergebnisse)
+save_results(results)
 
 print("Analyse abgeschlossen und Ergebnisse gespeichert.")
